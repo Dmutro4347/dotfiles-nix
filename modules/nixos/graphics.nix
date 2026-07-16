6403093c1c1ae4d9839f0d636e1ac8d96ef86b73
Dmytro Bui <dmutrobui@gmail.com>
2026-07-16 20:23:17 +0300

    rm graphics

--- modules/nixos/graphics.nix
+++
@@ -1,8 +0,0 @@
-{
-  flake.modules.nixos.graphics = {
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true; # потрібно для Steam і 32-бітних Vulkan/OpenGL лібів
-    };
-  };
-}
